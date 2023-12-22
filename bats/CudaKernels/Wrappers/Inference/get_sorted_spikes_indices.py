83e0de191fec4bc625c393dd7b6052f681d1c71f
AlexDeLos <alexdelos2001@gmail.com>
2023-12-22 08:42:48 +0100

    Update get_sorted_spikes_indices.py

--- bats/CudaKernels/Wrappers/Inference/get_sorted_spikes_indices.py
+++ bats/CudaKernels/Wrappers/Inference/get_sorted_spikes_indices.py
@@ -14,9 +14,10 @@ def get_sorted_spikes_indices(spike_times_per_neuron, n_spike_per_neuron):
     total_spikes = cp.sum(n_spike_per_neuron, axis=1)
     #max spikes a neuron got
     #! Error found here: 1 (found here when running the program on the cluster)
+    #!TODO: Here is where you start next time bro, GL <3
     max_total_spike = int(cp.max(total_spikes))
-    """sorted_indices = cp.argsort(spike_times_reshaped, axis=1)[:, :max_total_spike]"""
+    sorted_indices = cp.argsort(spike_times_reshaped, axis=1)[:, :max_total_spike]
     # creates keys? (was orriginally just called n)
     keys = np.arange(max_total_spike)
-    sorted_indices = cp.argpartition(spike_times_reshaped, keys, axis=1)[:, :max_total_spike]
+    # sorted_indices = cp.argpartition(spike_times_reshaped, keys, axis=1)[:, :max_total_spike] #!This was what "works"
     return new_shape, sorted_indices, spike_times_reshaped
