e991d6aab6f750956553b81ac49158013f08358a
AlexDeLos <alexdelos2001@gmail.com>
2024-01-22 14:34:41 +0100

    make the runs faster

--- experiments/mnist/train_spike_count.py
+++ experiments/mnist/train_spike_count.py
@@ -56,7 +56,7 @@ N_HIDDEN_LAYERS = 5
 
 # Training parameters
 N_TRAINING_EPOCHS = 10 #! used to  be 100
-N_TRAIN_SAMPLES = 60000 #! used to be 60000
+N_TRAIN_SAMPLES = 6000 #! used to be 60000
 N_TEST_SAMPLES = 10000 #! used to be 10000	
 TRAIN_BATCH_SIZE = 50 #! used to be 50
 TEST_BATCH_SIZE = 100
