60ce1b9edcafdac9408d7d489ad09ebf384f4f27
AlexDeLos <alexdelos2001@gmail.com>
2024-02-24 11:13:23 +0100

    Update train_conv.py

--- experiments/mnist/train_conv.py
+++ experiments/mnist/train_conv.py
@@ -173,9 +173,11 @@ for run in range(NUMBER_OF_RUNS):
 
     if not FIX_SEED:
         np_seed = int(cp.random.randint(low=0, high=max_int))
+        print('fixing seed np', np_seed)
     cp_seed = 2082618053
     if not FIX_SEED:
         cp_seed = int(cp.random.randint(low=0, high=max_int))
+        print('fixing seed cp', cp_seed)
 
     np.random.seed(np_seed)
     cp.random.seed(cp_seed)
