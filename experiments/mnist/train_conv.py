4fa87040802ba522d3e3424084126a9c8c78d362
AlexDeLos <alexdelos2001@gmail.com>
2024-02-26 11:08:26 +0100

    Update train_conv.py

--- experiments/mnist/train_conv.py
+++ experiments/mnist/train_conv.py
@@ -70,11 +70,11 @@ THRESHOLD_HAT_1_5 = 0.04
 DELTA_THRESHOLD_1_5 = 1 * THRESHOLD_HAT_1
 SPIKE_BUFFER_SIZE_1_5 = 10
 if USE_PADDING:
-    FILTER_FROM_NEXT_1_5 = np.array([3, 3, 20])
+    FILTER_FROM_NEXT_1_5 = np.array([3, 3, 10])
 else:
     FILTER_FROM_NEXT_1_5 = None
 
-FILTER_2 = np.array([3, 3, 20]) # used to be [5,5,40] -> is the 40 the channels?
+FILTER_2 = np.array([3, 3, 10]) # used to be [5,5,40] -> is the 40 the channels?
 TAU_S_2 = 0.130
 THRESHOLD_HAT_2 = 0.8
 DELTA_THRESHOLD_2 = 1 * THRESHOLD_HAT_2
@@ -84,7 +84,7 @@ if USE_PADDING:
 else:
     FILTER_FROM_NEXT_2 = None
 
-N_NEURONS_FC = 300
+N_NEURONS_FC = 100
 TAU_S_FC = 0.130
 THRESHOLD_HAT_FC = 0.6
 DELTA_THRESHOLD_FC = 1 * THRESHOLD_HAT_FC
