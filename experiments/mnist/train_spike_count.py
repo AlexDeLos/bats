8c72553c26b300fd6decb0049e0978e675e0027e
AlexDeLos <alexdelos2001@gmail.com>
2024-03-23 10:14:30 +0100

    Update train_spike_count.py

--- experiments/mnist/train_spike_count.py
+++ experiments/mnist/train_spike_count.py
@@ -80,8 +80,8 @@ N_TRAINING_EPOCHS = arguments.n_epochs #! used to  be 100
 if CLUSTER:
     N_TRAIN_SAMPLES = 60000 #! used to be 60000
     N_TEST_SAMPLES = 10000 #! used to be 10000
-    TRAIN_BATCH_SIZE = 50 #! used to be 50 -> putting it at 50 crashes the cluster when using append
-    TEST_BATCH_SIZE = 100
+    TRAIN_BATCH_SIZE = arguments.batch_size
+    TEST_BATCH_SIZE = arguments.batch_size_test
 else:
     N_TRAIN_SAMPLES = 600
     N_TEST_SAMPLES = 100
