a998702de5a965038c21ef7e86e5568dfc240b9e
AlexDeLos <alexdelos2001@gmail.com>
2024-04-06 12:02:37 +0200

    Update train_conv.py

--- experiments/mnist/train_conv.py
+++ experiments/mnist/train_conv.py
@@ -62,20 +62,20 @@ N_INPUTS = 28 * 28
 SIMULATION_TIME = 0.2
 
 FILTER_1 = np.array([5, 5, 15]) #? could it be the size of this filter's channels?
-TAU_S_1 = 0.30
-THRESHOLD_HAT_1 = 0.004
+TAU_S_1 = 0.3
+THRESHOLD_HAT_1 = 0.04
 DELTA_THRESHOLD_1 = 1 * THRESHOLD_HAT_1
 SPIKE_BUFFER_SIZE_1 = 10
 
 FILTER_1_5 = np.array([5, 5, 15]) #? could it be the size of this filter's channels?
-TAU_S_1_5 = 0.30
-THRESHOLD_HAT_1_5 = 0.004
+TAU_S_1_5 = 0.3
+THRESHOLD_HAT_1_5 = 0.04
 DELTA_THRESHOLD_1_5 = 1 * THRESHOLD_HAT_1
 SPIKE_BUFFER_SIZE_1_5 = 10
 
 FILTER_2 = np.array([5, 5, 20]) # used to be [5,5,40] -> is the 40 the channels?
-TAU_S_2 = 0.130
-THRESHOLD_HAT_2 = 0.008
+TAU_S_2 = 0.3
+THRESHOLD_HAT_2 = 0.04
 DELTA_THRESHOLD_2 = 1 * THRESHOLD_HAT_2
 SPIKE_BUFFER_SIZE_2 = 21
 
