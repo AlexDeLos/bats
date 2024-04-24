8a93640f2239607839c67e9309f3c50197695385
AlexDeLos <alexdelos2001@gmail.com>
2024-04-24 09:14:17 +0200

    Update utils.py

--- experiments/utils/utils.py
+++ experiments/utils/utils.py
@@ -67,7 +67,7 @@ class wandb_handler:
 
 
 
-def build_network_SNN(network, weight_initializer,n_input, n_hidden, neuron_var,neuron_out_var ,res_neuron_var, use_residual, res_every_n, res_jump_l, fuse_function):
+def build_network_SNN(network, weight_initializer,n_input, n_hidden, neuron_var,neuron_out_var ,res_neuron_var, use_residual, res_every_n, res_jump_l, fuse_function,use_delay):
     input_layer = InputLayer(n_neurons=n_input, name="Input layer")
     network.add_layer(input_layer, input=True)
     hidden_layers = []
@@ -90,6 +90,7 @@ def build_network_SNN(network, weight_initializer,n_input, n_hidden, neuron_var,
                                             n_neurons=res_neuron_var['n_neurons'], tau_s=res_neuron_var['tau_s'],
                                             theta=res_neuron_var['threshold_hat'],
                                             fuse_function=fuse_function,
+                                            use_delay = use_delay,
                                             delta_theta=res_neuron_var['delta_threshold'],
                                             weight_initializer=weight_initializer,
                                             max_n_spike=res_neuron_var['spike_buffer_size'],
