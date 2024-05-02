1f65c2ef69792072c815ebb34f376f43e29ce5ac
AlexDeLos <alexdelos2001@gmail.com>
2024-05-02 09:57:07 +0200

    Update utils.py

--- experiments/utils/utils.py
+++ experiments/utils/utils.py
@@ -33,7 +33,7 @@ class wandb_handler:
                 "use_delay": config['Use_delay'],
                 "loss function": config['loss'],
                 "architecture": "SNN",
-                "version": "1.1.2",
+                "version": "Testing Delayed Residuals 1.1.2",
             }
         else:
             self.config = {
