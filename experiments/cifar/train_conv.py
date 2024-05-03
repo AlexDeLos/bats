67ab95d9a0f3e9b0f3578e100751de0d8884d845
AlexDeLos <alexdelos2001@gmail.com>
2024-05-03 12:02:16 +0200

    Update train_conv.py

--- experiments/cifar/train_conv.py
+++ experiments/cifar/train_conv.py
@@ -258,6 +258,7 @@ for run in range(NUMBER_OF_RUNS):
     print("Training...")
     dic = Path("last"+ str(SAVE_DIR))
     if RESTORE and dic.exists():
+        print("""Restoring network from last save...""", dic)
         network.restore(dic)
     for epoch in range(N_TRAINING_EPOCHS):
         train_time_monitor.start()
