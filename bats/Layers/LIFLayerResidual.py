d7a5755340d9744a6b9cc3fd1e22798e362f52c7
Alex <alexdelos2001@gmail.com>
2024-05-30 20:30:41 +0200

    Update LIFLayerResidual.py

--- bats/Layers/LIFLayerResidual.py
+++ bats/Layers/LIFLayerResidual.py
@@ -192,18 +192,18 @@ class LIFLayerResidual(AbstractLayer):
 
         self.__weights_res += delta_weights
     
-    def store(self, dir_path: Path) -> None:
-        weights = self.weights
-        if weights is not None:
-            pre,_ = WEIGHTS_FILE_SUFFIX.split('.npy')
-            filename_res = dir_path / (self._name + pre + "_res" + '.npy')
-            np.save(filename_res, self.__weights_res.get())
+    # def store(self, dir_path: Path) -> None:
+    #     weights = self.weights
+    #     if weights is not None:
+    #         pre,_ = WEIGHTS_FILE_SUFFIX.split('.npy')
+    #         filename_res = dir_path / (self._name + pre + "_res" + '.npy')
+    #         np.save(filename_res, self.__weights_res.get())
 
-    def restore(self, dir_path: Path) -> None:
-        pre,_ = WEIGHTS_FILE_SUFFIX.split('.npy')
-        filename_res = dir_path / (self._name + pre + "_res" + '.npy')
-        if filename_res.exists():
-            self.__weights_res = np.load(filename_res)
+    # def restore(self, dir_path: Path) -> None:
+    #     pre,_ = WEIGHTS_FILE_SUFFIX.split('.npy')
+    #     filename_res = dir_path / (self._name + pre + "_res" + '.npy')
+    #     if filename_res.exists():
+    #         self.__weights_res = np.load(filename_res)
 
 
 
