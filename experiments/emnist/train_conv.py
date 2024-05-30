b9ed34ad8c07e40f91e8398b407f50e84e66a642
Alex <alexdelos2001@gmail.com>
2024-05-30 23:43:34 +0200

    Update train_conv.py

--- experiments/emnist/train_conv.py
+++ experiments/emnist/train_conv.py
@@ -65,14 +65,14 @@ INPUT_SHAPE = np.array([28, 28, 1])
 SIMULATION_TIME = 0.2
 CHANNELS = 16
 conv_var = {
-    'filter': np.array([5, 5, CHANNELS]),
+    'filter': np.array([3, 3, CHANNELS]),
     'tau_s': 0.130,
     'threshold_hat': 0.1,
     'delta_threshold': 1 * 0.1,
     'spike_buffer_size': 10
 }
 conv_res_var = {
-    'filter': np.array([5, 5, CHANNELS]),
+    'filter': np.array([3, 3, CHANNELS]),
     'tau_s': 0.130,
     'threshold_hat': 0.04,
     'delta_threshold': 1 * 0.04,
