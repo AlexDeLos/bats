5ffd36632a3d44604cf987c3e05b1b78a26ea462
Alex <alexdelos2001@gmail.com>
2024-06-06 07:23:49 +0200

    less filter in EMNIST

--- experiments/emnist/train_conv.py
+++ experiments/emnist/train_conv.py
@@ -65,18 +65,18 @@ INPUT_SHAPE = np.array([28, 28, 1])
 SIMULATION_TIME = 0.2
 CHANNELS = 16
 conv_var = {
-    'filter': np.array([5, 5, CHANNELS]),
+    'filter': np.array([3, 3, CHANNELS]),
     'tau_s': 0.130,
     'threshold_hat': 0.1,
     'delta_threshold': 1 * 0.1,
-    'spike_buffer_size': 5
+    'spike_buffer_size': 2
 }
 conv_res_var = {
-    'filter': np.array([5, 5, CHANNELS]),
+    'filter': np.array([3, 3, CHANNELS]),
     'tau_s': 0.130,
     'threshold_hat': 0.5,
     'delta_threshold': 1 * 0.5,
-    'spike_buffer_size': 5
+    'spike_buffer_size': 2
 }
 fc_var = {
     'n_neurons': 300,
