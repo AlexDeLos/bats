90dfe84ad4f0fac3c3f19ec6ae019ff90f2d63f8
Alex <alexdelos2001@gmail.com>
2024-06-08 08:12:19 +0200

    added learning rate decay

--- experiments/mnist/train.py
+++ experiments/mnist/train.py
@@ -101,7 +101,7 @@ if arguments.learning_rate is not None:
 else:
     LEARNING_RATE = 0.0005#arguments.learning_rate
 LR_DECAY_EPOCH = int(N_TRAINING_EPOCHS/10)  # Perform decay very n epochs
-LR_DECAY_FACTOR = 1.0
+LR_DECAY_FACTOR = 0.75
 MIN_LEARNING_RATE = 0
 TARGET_FALSE = 3
 TARGET_TRUE = out_buffer_size
