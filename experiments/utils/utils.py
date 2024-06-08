51aeddfa395ed3fc70aa2bd9ba9253941500dd44
Alex <alexdelos2001@gmail.com>
2024-06-08 08:18:35 +0200

    Update utils.py

--- experiments/utils/utils.py
+++ experiments/utils/utils.py
@@ -35,7 +35,7 @@ class wandb_handler:
                 "use_delay": config['Use_delay'],
                 "loss function": config['loss'],
                 "architecture": "SNN",
-                "version": "1.4.3",
+                "version": "1.4.4",
             }
         else:
             self.config = {
