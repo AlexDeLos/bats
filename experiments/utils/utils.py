35638ed78e5ba0694b23f8d9ec4192bad453cb7c
Alex <64327268+AlexDeLos@users.noreply.github.com>
2024-07-08 15:56:37 -0800

    Update utils.py

--- experiments/utils/utils.py
+++ experiments/utils/utils.py
@@ -66,7 +66,7 @@ class wandb_handler:
                 "false target": config['False_target'],
                 "loss function": config['loss'],
                 "use_delay": config['Use_delay'],
-                "version": "1.5.3",
+                "version": "1.5.4",
             }
         self.run = wandb.init(project=project_name, name=experiment_name, config=self.config)
         # self.run = None
